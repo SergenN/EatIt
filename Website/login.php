cd51ad0569f8d5c38145e5d17d8cd892280bdbb9
Thijsku <thijs.kuilman@gmail.com>
2014-10-15 01:05:28 +0200

    Registreren en inloggen WIP

--- Website/login.php
+++ Website/login.php
@@ -1,9 +1,69 @@
-<form class="form-signin" role="form">
-        <h2 class="form-signin-heading">Inloggen</h2>
-        <input type="email" class="form-control" placeholder="Email address" required autofocus>
-        <input type="password" class="form-control" placeholder="Password" required>
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      </form>
+<?php 
+	// Create connection
+	$con=mysqli_connect("localhost","root","","eatit");
+
+	// Check connection
+	if (mysqli_connect_errno()) {
+	  echo "Kan niet verbinden met de database: " . mysqli_connect_error();
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Inloggen</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+<body>
+
+<?php
+// De variabelen die in de invoervelden worden aangepast
+$l_email = "";
+$l_password = "";
+$password = "1";
+
+if (isset($_POST['l_submit'])) {
+
+	if(isset($l_email)){
+	$l_email = $_POST['l_email'];
+	}
+
+	if(isset($l_email)){
+		$l_password = $_POST['l_password'];
+	}
+	
+	// Kijken of het email adres al in de database staat. Dit wordt gedaan door het ingevulde email adres m.b.v een WHERE en een COUNT statement te tellen. Als er 0 zijn, dan kan de persoon zich registeren. Als het op 1 staat, dan volgt er een error.
+	$query = "SELECT wachtwoord FROM klant WHERE email = '" . $l_email . "' ";
+	$result = mysqli_query($con, $query);
+	$password = '';
+
+	echo $query;
+
+	while($row = mysqli_fetch_array($result)) {
+		$password = $row['wachtwoord'];
+	}
+}
+?>
+<div class="login">
+	<center>
+	<img src="img/logo_notext.png" action="registreren.php" class="logo">
+	<form class="form-signin" method="post">
+	<h2>Inloggen</h2>  
+	<?php
+	 if($password == $l_password && isset($_POST['l_submit'])){
+		echo '<div class="success">Succesvol ingelogd!</div><br>';
+	}elseif($password != $l_password && isset($_POST['l_submit'])){
+		echo '<div class="error">Onjuiste gegevens ingevoerd</div><br>';
+	}
+	?>
+	  <a href="register.php">Nog geen lid? Registreer je hier!</a><br><br>
+	    <input type="email" class="invoerveld" name="l_email" placeholder="Email" required autofocus value=<?php echo '"' . $l_email . '"'; ?>><br><br>
+	    <input type="password" class="invoerveld" name="l_password" placeholder="Wachtwoord" required value=<?php echo '"' . $l_password . '"'; ?>><br><br>
+
+	    <br>
+	    <button type="submit" name="l_submit" id="submit">Inloggen</button>
+	</form>
+	</center>
+</div>
+
+</body>
--- Website/register.php
+++ Website/register.php
@@ -25,11 +25,11 @@ $telefoonnummer = "";
 $plaats = "";
 $adres = "";
 $postcode = "";
+$emailcheck = 0;
 
 // Kijken of de variables zijn invuld. Zoja: zet de postdata om naar de variabelen
 if (isset($_POST['email'])) {
 	$email = $_POST['email'];
-	echo $email;
 }
 
 if (isset($_POST['voornaam'])) {
@@ -56,29 +56,57 @@ if (isset($_POST['postcode'])) {
 	$postcode = $_POST['postcode'];
 }
 
-// Debug
-echo 
-$email . " | " .
-$voornaam . " | " .
-$achternaam . " | " .
-$telefoonnummer . " | " .
-$plaats . " | " .
-$adres . " | " .
-$postcode;
 
-// Voorwaarden bekijken van variables
-// if(isset($_POST['submit'])){
-// 	$query = "INSERT INTO klant VALUES (henk, hoi)";
-// 	$result = mysqli_query($con, $query);
-// }
+// Het account aanmaken zodra er op het knopje is geklikt.
+if(isset($_POST['submit'])){
+	// Kijken of het email adres al in de database staat. Dit wordt gedaan door het ingevulde email adres m.b.v een WHERE en een COUNT statement te tellen. Als er 0 zijn, dan kan de persoon zich registeren. Als het op 1 staat, dan volgt er een error.
+	$query = "SELECT count(email) FROM klant WHERE email = '" . $email . "' ";
+	$result = mysqli_query($con, $query);
+
+
+	while($row = mysqli_fetch_array($result)) {
+		$emailcheck = $row['count(email)'];
+	}
+
+	// Als het email adres niet gevonden is, dan worden de gegevens in de database gezet
+	if($emailcheck == 0){
+		// Een inlogcode (oftewel: wachtwoord) genereren
+
+		// Uit deze letters kan het script kiezen
+		$letters = '123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+		// De variabel die de gegenereerde code opslaat 
+		$secretcode = '';
+
+		// Een loop die telkens een willekeurige positie van $letters pakt en dit aan de code toevoegt
+		for ($i=0; $i < 10; $i++) { 
+			$secretcode .= $letters[rand(0,strlen($letters) - 1)];
+		}
+
+		// De geheime code door een hash halen, waardoor het nog moeilijker te kraken is
+		$secretcode = hash('crc32b', $secretcode);
+
+		// De gegevens invullen
+		$query = "INSERT INTO klant VALUES ('" . $email . "', '" . $voornaam . "', '" . $achternaam . "', '" . $telefoonnummer . "', '" . $plaats . "', '" . $adres . "', '" . $postcode . "', '" . $secretcode . "')";
+		$result = mysqli_query($con, $query);
+	}
+}
 
 ?>
 
+<!-- Het formulier waarin de invoervelden staan om je te registreren -->
 <div class="login">
 	<center>
 	<img src="img/logo_notext.png" action="registreren.php" class="logo">
 	<form class="form-signin" method="post">
-	  <h2>Registreren</h2><br>
+	  <h2>Registreren</h2>
+	  <a href="login.php">Al lid? Log hier in!</a><br><br>
+	  	<?php
+	  		// Errorcode weergeven wanneer een email in bezet is
+			if($emailcheck == 1 && isset($_POST['submit'])){
+				echo '<div class="error">Dit email adres is al in gebruik.</div>';
+			}
+		?>
 	    <input type="email" class="invoerveld" name="email" placeholder="Email" required autofocus value=<?php echo '"' . $email . '"'; ?>><br><br>
 	    <input type="text" class="invoerveld" name="voornaam" placeholder="Voornaam" required value=<?php echo '"' . $voornaam . '"'; ?>><br><br>
 	    <input type="text" class="invoerveld" name="achternaam" placeholder="Achternaam" required value=<?php echo '"' . $achternaam . '"'; ?>><br><br>
@@ -87,7 +115,7 @@ $postcode;
 	    <input type="text" class="invoerveld" name="adres" placeholder="Adres" required value=<?php echo '"' . $adres . '"'; ?>><br><br>
 	    <input type="text" class="invoerveld" name="postcode" placeholder="Postcode" required value=<?php echo '"' . $postcode . '"'; ?>><br><br>
 
-	    <br><br><br>
+	    <br>
 	    <button type="submit" name="submit" id="submit">Aanmelding voltooien</button>
 	</form>
 	</center>
