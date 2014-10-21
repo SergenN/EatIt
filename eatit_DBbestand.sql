c925a7be03ba9df3887f546fffb4976adeefc26c
Thijsku <thijs.kuilman@gmail.com>
2014-10-21 14:58:45 +0200

    Nieuw Database bestand voor klanten

--- eatit_DBbestand.sql
+++ eatit_DBbestand.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2014 at 04:45 PM
+-- Generation Time: Oct 21, 2014 at 02:57 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -27,6 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `klant` (
+  `klantennummer` int(100) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   `voornaam` text NOT NULL,
   `achternaam` text NOT NULL,
@@ -35,8 +36,20 @@ CREATE TABLE IF NOT EXISTS `klant` (
   `adres` text NOT NULL,
   `postcode` varchar(40) NOT NULL,
   `wachtwoord` varchar(40) NOT NULL,
-  PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `permissies` text NOT NULL,
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `klantennummer` (`klantennummer`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `klant`
+--
+
+INSERT INTO `klant` (`klantennummer`, `email`, `voornaam`, `achternaam`, `telefoonnummer`, `plaats`, `adres`, `postcode`, `wachtwoord`, `permissies`) VALUES
+(1, '3thijs.kuilman@gmail.com', 'ok', 'ok', 0, '90', '09', '09', '123456', 'lid'),
+(3, 'eend@assdf.com', '89', '89', 89, '89', '89', '89', '3637fd38', 'lid'),
+(4, 'haas@konijn.nl', 'Konijn', 'haas', 61161616, 'efeok', 'ok', 'od', '123456', 'lid'),
+(2, 'sadasd@adsfs.com', '89', '89', 89, '89', '89', '89', '3e8d3bf6', 'lid');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
