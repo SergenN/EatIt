6a1b95256c686b679529e39f9e8632d54e20010e
Thijsku <thijs.kuilman@gmail.com>
2014-11-05 22:12:26 +0100

    Winkelwagen nieuwste versie

--- WebsiteV2/inc/template/winkelwagen.php
+++ WebsiteV2/inc/template/winkelwagen.php
@@ -1,12 +1,18 @@
 <div class="content">
     <?php
+
+    // Winkelwagen is alleen te gebruiken voor klanten.
+	if($_SESSION['soortgebruiker'] != "klant"){
+		header ('location: index.php');
+	}
+
     if(isset($_GET['res'])){
-        if($_GET['res'] == 'succes'){
-            echo '<div class="success">Uw heeft besteld!</div><br>';
+        if($_GET['res'] == 'success'){
+            echo '<div class="success"><center>U heeft uw bestelling geplaatst!</center></div><br>';
         } else if($_GET['res'] == 'error'){
-            echo '<div class="error">Oeps er ging iets fout</div><br>';
+            echo '<div class="error"><center>Oeps er ging iets fout</center></div><br>';
         } else if($_GET['res'] == 'fail') {
-            echo '<div class="success">U kunt artikel '.$_GET['gid'].' niet meer bestellen, want die hebben wij niet meer op voorraad!</div><br>';
+            echo '<div class="success"><center>U kunt artikel '.$_GET['gid'].' niet meer bestellen, want die hebben wij niet meer op voorraad!</center></div><br>';
         }
     }
 
@@ -25,8 +31,8 @@
             }
             while ($naam = mysqli_fetch_assoc($result2)) {
 
-                echo "u heeft van het gerecht {$naam['GER_Naam']}, $aantal besteld";
-                echo "<hr/>";
+            echo "Het gerecht " . $naam['GER_Naam'] . " is " . $aantal. " keer door U besteld";
+            echo "<hr/>";
             }
         }
 //als er geen gerechten besteld zijn wordt dat weergegeven
@@ -36,8 +42,8 @@
     //als er gerechten besteld zijn wordt de verwijder knop weergegeven
     if(isset($_SESSION['bes'])){
         echo "		<form action=\"\" method=\"post\">
-					<input type=\"submit\" name=\"delete\"  value=\"verwijderen\" />
-				</form>";
+					<input type=\"submit\" name=\"delete\" class=\"submit\" value=\"Verwijderen\" />
+				</form><br>";
     }
 
 
@@ -45,9 +51,9 @@
     <!-- als er gerechten besteld zijn wordt de bevestig knop weergegeven-->
     <form action="" method="post">
         <?php 	if(isset($_SESSION['bes'])) {
-            echo"<input type=\"submit\" name=\"confirm\" value=\"bevestigen\"/>";
+            echo"<input type=\"submit\" name=\"confirm\" class=\"submit\" value=\"Bevestigen\"/>";
         } ?>
-        <input type="submit" name="back" class="button" value="terug naar bestellen"/>
+        <br><br><input type="submit" name="back" class="submit" value="Terug naar bestellen"/>
     </form>
 
     <?php
@@ -112,4 +118,58 @@
     }
 
     ?>
+
+    <!-- ADRES WIJZIGEN -->
+<?php
+
+// Een functie die ervoor zorgt dat de Klantgegevens worden aangepast in de database
+function update_klant($name, $email, $con, $newvalue){
+	// De update query
+	$query = "UPDATE Klant SET " . $name . "= '" . $newvalue . "' WHERE KL_Mail='" . $email . "'";
+	$result = mysqli_query($con, $query);
+
+	// Aangeven dat de aanpassingen succesvol zijn
+	echo '<center><div class="success">' . ucfirst($name) . ' is succesvol aangepast!</div></center>';
+}
+
+
+// Als er een klant is ingelogd, dan krijgt hij/zij het volgende te zien:
+if($_SESSION['soortgebruiker'] == "klant"){
+	// print_r($gegevens);
+	if(isset($_POST['submit_settings'])){
+
+		if ($_POST['plaats'] != '') {
+			$plaats = $_POST['plaats'];
+			update_klant('KL_Plaats', $gegevens['KL_Mail'], $con, $plaats);
+		}
+
+		if ($_POST['adres'] != '') {
+			$adres = $_POST['adres'];
+			update_klant('KL_Adres', $gegevens['KL_Mail'], $con, $adres);
+		}
+
+		if ($_POST['postcode'] != '') {
+			$postcode = $_POST['postcode'];
+			update_klant('KL_Postcode', $gegevens['KL_Mail'], $con, $postcode);
+		}
+	}		
+?>
+
+	<!-- De content. Hier komt alle inhoud van de site. -->
+	<div class="content">
+		<!-- Het formulier -->
+		<h2>Adresgegevens wijzigen (optioneel)</h2>
+		<!-- Alle invoervelden voor het wijzigen van de persoonlijke gegevens. Niets is required, dus velden kunnen ook worden overgeslagen als ze hetzelfde moeten blijven. -->
+		<form class="form-signin" name="changedata" method="post" action="?p=winkelwagen">
+		Plaats:<br><input type="text" class="invoerveld" name="plaats" placeholder=<?php echo '"' . $gegevens['KL_Plaats'] . '"'; ?>><br><br>
+		Adres + Huisnummer:<br><input type="text" class="invoerveld" name="adres" placeholder=<?php echo '"' . $gegevens['KL_Adres'] . '"'; ?>><br><br>
+		Postcode:<br><input type="text" class="invoerveld" name="postcode" placeholder=<?php echo '"' . $gegevens['KL_Postcode'] . '"'; ?>><br><br>
+
+		<!-- Het formulier verzenden. -->
+		<button type="submit" name="submit_settings" class="submit">Wijzigingen opslaan</button>
+		</form>
+	</div>
+<?php } ?>
+
+</div>	
 </div>
