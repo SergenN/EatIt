4bd6041c049cc63116544cd244caac736cb4affb
Sergen-Nurel <sergen_nurel@hotmail.com>
2014-11-05 12:27:10 +0100

    Keuken scherm aangemaakt.

---
+++ WebsiteV2/inc/template/keuken.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sergen Nurel
+ * Date: 5-11-2014
+ * Time: 11:28
+ */
+
+function getOrders() {
+    global $con;
+    $toret = "";
+    $query = "SELECT * FROM bestelling WHERE BEST_status='besteld';";
+    $result = mysqli_query($con, $query);
+    while ($row = mysqli_fetch_assoc($result)) {
+        //Alle variabelen $row = {BestNR, KlantNR, MedNR, BEST_Datum, BEST_Status}
+        $toret .= '<li>';
+        $toret .= '<p class="lijstTitle">Bestelling: '.$row['BestNR'].' | Klant: '.$row['KlantNR'].'</p>';
+        $query = "SELECT * FROM AantalVerkocht a JOIN Gerecht g ON a.GerNR = g.GerNR WHERE BestNR = {$row['BestNR']};";
+        $result2 = mysqli_query($con, $query);
+        while($row2 = mysqli_fetch_assoc($result2)){
+            //Alle variabelen $row2 = {BestNR, GerNr, Aantal = Ger_NR, Ger_Naam, Ger_Prijs, Ger_Beschrijving}
+
+            $toret .= '<p class="lijstSub">Gerecht: '.$row2['GerNR'].' '. $row2['Ger_Prijs'].' | Aantal: '. $row2['Aantal'].'</p>';
+
+            $query = "SELECT * FROM Aantalingredienten ai JOIN Artikelen ar ON ai.ArtNR = ar.ArtNR WHERE GerNR = {$row2['GerNR']};";
+            $result3 = mysqli_query($con, $query);
+
+            $toret .= '<p class="lijstIngredienten">';
+            while($row3 = mysqli_fetch_assoc($result3)){
+                //Alle variabelen $row3 = {AiNR, GerNR, ArtNR, IngAantal = ArtNR, ArtNaam}
+                $toret .=  'Artikel:'.$row3['ArtNR'].' '. $row3['ArtNaam'] .' Hoeveelheid: '.$row3['IngAantal'].'<br>';
+            }
+            $toret .= '</p>';
+        }
+        $toret .= "</li>";
+    }
+    return $toret;
+}
+
+?>
+
+<div class="content">
+    <ul class="gerechtLijst">
+        <?php
+        echo getOrders();
+        ?>
+    </ul>
+
+</div>
