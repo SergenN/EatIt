1f7a4138ad88d0cc3c1b32cb4757134570824862
Thijsku <thijs.kuilman@gmail.com>
2014-11-06 03:08:14 +0100

    Verbeteringen en stuff

--- WebsiteV2/inc/template/inkoopfactuur.php
+++ WebsiteV2/inc/template/inkoopfactuur.php
@@ -13,38 +13,40 @@ if(isset($_GET['res'])){
 if(isset($_GET['nummer'])){
 
     $levNr = $_GET['nummer'];
-    $query1 = "SELECT * FROM Inkoopfactuur if JOIN Inkooporder io ON io.LevNR = if.InkfNR JOIN Bestelorder bo ON bo.OrderNR = io.OrderNR JOIN Artikelen a ON a.ArtNR = bo.ArtNR WHERE if.InkfNR = $levNr;";
+    $query1 = "SELECT * FROM Inkoopfactuur i JOIN Inkooporder io ON io.LevNR = i.InkfNR JOIN Bestelorder bo ON bo.OrderNR = io.OrderNR JOIN Artikelen a ON a.ArtNR = bo.ArtNR WHERE i.InkfNR = $levNr;";
     $result = mysqli_fetch_assoc(mysqli_query($con, $query1));
+
     if(mysqli_error($con) || empty($result)){
-        header('location: index.php?p=inkoopfacatuur&res=failed');
+        header('location: index.php?p=inkoopfactuur&res=failed');
     }
 
     $artNR = $result['ArtNR'];
-    $newTV = $result['TechnischeVoorraad'] + ['Aantal'];
-    $newIB = $result['InBestelling'] - ['Aantal'];
+    $newTV = $result['ART_TechnischeVoorraad'] + $result['Aantal'];
+    $newIB = $result['ART_InBestelling'] - $result['Aantal'];
 
-    $query2 = "UPDATE Artikelen SET TechnischeVoorraad=$newTV, InBestelling=$newIB WHERE ArtNR = $artNR;";
+    $query2 = "UPDATE Artikelen SET ART_TechnischeVoorraad=$newTV, ART_InBestelling=$newIB WHERE ArtNR = $artNR;";
     mysqli_query($con, $query2);
     if(mysqli_error($con)){
-        header('location: index.php?p=inkoopfacatuur&res=failed');
+        header('location: index.php?p=inkoopfactuur&res=failed');
     }
 
     $query3 = "UPDATE Inkoopfactuur SET Inkf_Status='geleverd' WHERE InkfNR = $levNr;";
     mysqli_query($con, $query3);
-	header('location: index.php?p=inkoopfacatuur&res=success');
+	header('location: index.php?p=inkoopfactuur&res=success');
 }
 
-$query  = "select InkfNR, Inkf_Status, Bedrag, OrderNR, IngNR, LevNR, Aantal ";
-$query .= "from Inkooporder o, Inkoopfactuur f ";
-$query .= "where o.OrderNR = f.InkfNR ORDER BY Inkf_Status ASC; ";
+$query  = "select * ";
+$query .= "from Inkooporder o ";
+$query .= "JOIN Inkoopfactuur f ON o.LevNR = f.InkfNR ORDER BY Inkf_Status ASC; ";
+
 $result = mysqli_query($con,$query);
 if(!$result){
-    header('location: index.php?p=inkoopfacatuur&res=failed');
+    header('location: index.php?p=inkoopfactuur&res=failed');
 }
 
 while ($row = mysqli_fetch_assoc($result)) {
 	echo "Factuurnummer: " . $row['InkfNR'] . "<br/>Factuurstatus: " . $row["Inkf_Status"] . "<br/>" . "Ordernummer: " . $row["OrderNR"] . "<br/>Bedrag: €" . $row["Bedrag"];
-	echo "<br/>Leveranciernummer: " . $row["LevNR"] . "<br/>Ingredientnummer: " . $row["IngNR"] .  "<br/>Aantal: " . $row["Aantal"];
+	echo "<br/>Ingredientnummer: " . $row["IngNR"] .  "<br/>Aantal: " . $row["Aantal"];
 
 	if($row['Inkf_Status'] == 'besteld'){ 
 		echo "<br><br><a href=\"?p=inkoopfactuur&nummer=" . $row["InkfNR"] . "\"> ^ Aftekenen</a><br><br>";
