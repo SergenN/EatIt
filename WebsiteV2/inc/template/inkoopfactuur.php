5f14b3a96b9b4cc3bfe1880c3b8cf47d5fd2cc23
Thijsku <thijs.kuilman@gmail.com>
2014-11-06 01:40:53 +0100

    Inkoopfactuur. CADEAU

--- WebsiteV2/inc/template/inkoopfactuur.php
+++ WebsiteV2/inc/template/inkoopfactuur.php
@@ -1,9 +1,13 @@
 <div class="content">
 <?php
 
+if(isset($_GET['nummer'])){
+	
+}
+
 $query  = "select InkfNR, Inkf_Status, Bedrag, OrderNR, IngNR, LevNR, Aantal ";
 $query .= "from Inkooporder o, Inkoopfactuur f ";
-$query .= "where o.OrderNR = f.InkfNR; ";
+$query .= "where o.OrderNR = f.InkfNR ORDER BY Inkf_Status ASC; ";
 $result = mysqli_query($con,$query);
 if(!$result){
 	die("Query failed");
@@ -12,6 +16,11 @@ if(!$result){
 while ($row = mysqli_fetch_assoc($result)) {
 	echo "Factuurnummer: " . $row['InkfNR'] . "<br/>Factuurstatus: " . $row["Inkf_Status"] . "<br/>" . "Ordernummer: " . $row["OrderNR"] . "<br/>Bedrag: €" . $row["Bedrag"];
 	echo "<br/>Leveranciernummer: " . $row["LevNR"] . "<br/>Ingredientnummer: " . $row["IngNR"] .  "<br/>Aantal: " . $row["Aantal"];
+
+	if($row['Inkf_Status'] == 'besteld'){ 
+		echo "<br><br><a href=\"?p=inkoopfactuur&nummer=" . $row["InkfNR"] . "\"> ^ Aftekenen</a><br><br>";
+	}
+
 	echo "<hr/>";
 }
 
